8915224a63f7ff66ed91ced2a10a017c334d343a
nobody <1395759132@qq.com>
2024-08-08 22:20:44 +0800

    类加载器

---
+++ src/main/java/com/xj/demo/ClassInit.java
@@ -0,0 +1,18 @@
+package com.xj.demo;
+
+public class ClassInit {
+    private static int i = 1;
+    static {
+        i = 2;
+        j = 3;
+    }
+    private static int j = 311;
+    public static void main(String[] args) {
+        new ClassInit();
+        System.out.println(j);
+    }
+
+    public ClassInit() {
+        int a = 1;
+    }
+}
